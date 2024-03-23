d56f3b9995e7037bd8ee4d1b40833ec5ddd6a210
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2024-03-23 22:29:48 +0900

    Title: n개 중에 m개 뽑기, Time: 259ms, Memory: 25MB, Status: Passed - CodeTree

--- 240323/n개 중에 m개 뽑기/n-choose-m.py
+++ 240323/n개 중에 m개 뽑기/n-choose-m.py
@@ -3,21 +3,18 @@ N, M = list(map(int,input().split()))
 # N개 숫자 중 M개 고르기
 
 ans = []
-answers = []
 
 def select(idx,cnt):
 
     if idx == N :
         if cnt == M :
-            answers.append(ans[:])
+            print(*ans)
         return
-
-    select(idx+1,cnt)
-    
+            
     ans.append(idx+1)
     select(idx+1,cnt+1)
     ans.pop()
 
+    select(idx+1,cnt)
+
 select(0,0)
-for elem in sorted(answers):
-    print(*elem)
