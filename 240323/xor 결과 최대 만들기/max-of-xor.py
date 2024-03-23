966527840860dbf40ef2ea389c6deca7c9a7b873
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2024-03-23 22:43:52 +0900

    Title: xor 결과 최대 만들기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240323/xor 결과 최대 만들기/max-of-xor.py
@@ -0,0 +1,24 @@
+n,m = list(map(int,input().split()))
+
+selected = []
+answer = -1 
+def selectNum(curr_num, cnt):
+    global answer
+
+    if curr_num == n + 1:
+        if cnt == m :
+           xor = 1 
+           for elem in selected:
+                xor ^= elem
+           answer = max(xor,answer)
+        return
+
+    selected.append(curr_num)
+    selectNum(curr_num+1,cnt + 1)
+    selected.pop()
+
+    selectNum(curr_num+1,cnt)
+
+
+selectNum(1,0)
+print(answer)
