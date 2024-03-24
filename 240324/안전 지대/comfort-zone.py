8b8409ca3ef8e9c24934b96a46ca42b33bec3398
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2024-03-24 15:51:12 +0900

    Title: 안전 지대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240324/안전 지대/comfort-zone.py
@@ -0,0 +1,42 @@
+N, M = list(map(int,input().split()))
+
+grid = [list(map(int,input().split())) for _ in range(N)]
+
+dxs,dys = [-1,1,0,0],[0,0,-1,1]
+maxHeight = 0
+for i in range(N):
+    for j in range(M):
+        maxHeight = max(maxHeight,grid[i][j])
+
+def canGo(x,y,curHeight):
+
+    return not visited[x][y] and grid[x][y] > curHeight
+
+def inRange(x,y):
+
+    return 0<=x<N and 0<=y<M
+
+def dfs(cx,cy,height):
+
+    for dx,dy in zip(dxs,dys):
+        nx,ny = cx+dx,cy+dy
+        if inRange(nx,ny):
+            if canGo(nx,ny,height):
+                visited[nx][ny] = True
+                dfs(nx,ny,height)
+
+safeRegions = []
+
+for height in range(1,maxHeight):
+    visited = [[False] * M for _ in range(N)]
+    regionCnt = 0
+    for i in range(N):
+        for j in range(M):
+            if not visited[i][j] and grid[i][j] > height:
+                visited[i][j] = True
+                dfs(i,j,height)
+                regionCnt += 1
+    safeRegions.append((regionCnt,height))
+
+safeRegions.sort(key = lambda x : (-x[0],x[1]))
+print(*list(safeRegions[0]))
