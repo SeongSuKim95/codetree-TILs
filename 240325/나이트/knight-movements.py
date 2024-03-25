c41cc6789adf1bcc1a2db1413649122f4cfb3981
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2024-03-25 13:59:28 +0900

    Title: 나이트, Time: 143ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240325/나이트/knight-movements.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+N = int(input())
+
+sx,sy,ex,ey = list(map(lambda x : int(x) - 1,input().split()))
+
+dxs,dys = [-2,-1,1,2,2,1,-1,-2],[1,2,2,1,-1,-2,-2,-1]
+
+visited = [[False] * N for _ in range(N)]
+
+grid = [[0]* N for _ in range(N)]
+
+def inRange(x,y):
+
+    return 0<=x<N and 0<=y<N
+
+def bfs():
+    visited[sx][sy] = True
+    q = deque([(sx,sy)])
+    while q :
+        cx, cy = q.popleft()
+        for dx, dy in zip(dxs,dys):
+            nx, ny = cx + dx, cy+ dy
+            if inRange(nx,ny):
+                if not visited[nx][ny] :
+                    visited[nx][ny] = True
+                    grid[nx][ny] = grid[cx][cy] + 1
+                    if (nx,ny) == (ex,ey):
+                        return
+                    q.append((nx,ny))
+bfs()
+print(grid[ex][ey])
