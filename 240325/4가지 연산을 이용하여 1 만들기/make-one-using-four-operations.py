ecd7cb8fe18a771c8716f9cf79c89ee76dd5134d
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2024-03-25 16:02:21 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240325/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 240325/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -1,28 +1,39 @@
 from collections import deque
 
 N = int(input())
+nums = set()
 
 def bfs():
 
     q = deque()
     q.append((N,0))
+    nums.add(q)
+
     while q :
         curNum,curCnt = q.popleft()
         if curNum == 1 :
             return curCnt
 
         nextNum = curNum - 1 
-        q.append((nextNum,curCnt+1))
+        if nextNum not in nums:
+            q.append((nextNum,curCnt+1))
+            nums.add(nextNum)
 
         nextNum = curNum + 1
-        q.append((nextNum,curCnt+1))
-        
+        if nextNum not in nums:
+            q.append((nextNum,curCnt+1))
+            nums.add(nextNum)        
+
         if curNum % 2 == 0:
             nextNum = curNum // 2
-            q.append((nextNum,curCnt+1))
+            if nextNum not in nums:
+                q.append((nextNum,curCnt+1))
+                nums.add(nextNum)
 
         if curNum % 3 == 0:
             nextNum = curNum // 3
-            q.append((nextNum,curCnt+1))
+            if nextNum not in nums:
+                q.append((nextNum,curCnt+1))
+                nums.add(nextNum)
 
 print(bfs())
