16fb30cb0f68a6464fcfd3ed92051faa4b82529b
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2024-03-25 13:38:36 +0900

    Title: 최소 경로로 탈출 하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240325/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+N, M = list(map(int,input().split()))
+
+grid = [list(map(int,input().split())) for _ in range(N)]
+
+dxs,dys = [-1,1,0,0],[0,0,-1,1]
+visited = [[False] * N for _ in range(N)]
+
+def inRange(x,y):
+    
+    return 0<=x<N and 0<=y<N
+
+def bfs():
+
+    q = deque([(0,0)])
+    visited[0][0] = True
+    while q: 
+        cx,cy = q.popleft()
+        for dx,dy in zip(dxs,dys):
+            nx,ny = cx+dx, cy+dy
+            if inRange(nx,ny):
+                if not visited[nx][ny] and grid[nx][ny]:
+                    visited[nx][ny] = True
+                    grid[nx][ny] = grid[cx][cy] + 1
+                    q.append((nx,ny))
+
+bfs()
+if visited[N-1][N-1]:
+    print(grid[N-1][N-1]-1)
+else :
+    print(-1)
