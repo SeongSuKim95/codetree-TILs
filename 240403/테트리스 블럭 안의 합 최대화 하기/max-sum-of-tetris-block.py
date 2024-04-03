9f1b7b7f02366433c0c895e466afab4de3c51126
Kim Seong Su <62092317+SeongSuKim95@users.noreply.github.com>
2024-04-03 23:10:24 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 2063ms, Memory: 29MB, Status: Passed - CodeTree

--- 240403/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
+++ 240403/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
@@ -1,60 +1,50 @@
-N, M = list(map(int,input().split()))
-
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
 grid = [
-    list(map(int,input().split()))
-    for _ in range(N)
+    list(map(int, input().split()))
+    for _ in range(n)
 ]
 
-dxs,dys = [-1,1,0,0],[0,0,-1,1]
+visited_pos = []
+max_sum = 0
 
-visited = [
-    [False] * M 
-    for _ in range(N)
-]
 
-def inRange(x,y):
+# 주어진 위치가 격자를 벗어나는지 여부를 반환합니다.
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < m
 
-    return 0<=x<N and 0<=y<M
 
-block = []
-answer = 0
+# 주어진 위치로 이동할 수 있는지 여부를 확인합니다.
+def can_go(x, y):
+    return in_range(x, y) and (x, y) not in visited_pos
 
-def choose(cx,cy,cnt):
-    global answer
 
-    if cnt == 3 :
-        blockSum = 0
-        for bx,by in block:
-            blockSum += grid[bx][by]
-        answer = max(answer,blockSum)
-        return    
+# 주어진 위치에 대하여 가능한 모든 모양을 탐색하며 최대 합을 반환합니다.
+def get_max_sum(cnt, sum_of_nums):
+    if cnt == 4:
+        global max_sum
+        max_sum = max(max_sum, sum_of_nums)
+        return
     
-    for i in range(4):
-        
-        nx,ny = cx + dxs[i], cy + dys[i]
-        
-        if inRange(nx,ny) and not visited[nx][ny]:
-            
-            block.append((nx,ny))
-            visited[nx][ny] = True
-            choose(nx,ny,cnt+1)
-            block.pop()
-            visited[nx][ny] = False
-            
-            # if cnt == 1:
-
-            #     block.append((nx,ny))
-            #     visited[nx][ny] = True
-            #     choose(cx,cy,cnt+1)
-            #     block.pop()
-            #     visited[nx][ny] = False
+    dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
     
-for cx in range(N):
-    for cy in range(M):
-        block.append((cx,cy))
-        visited[cx][cy] = True
-        choose(cx,cy,0)
-        block.pop()
-        visited[cx][cy] = False
+    for (x, y) in visited_pos:
+        for dx, dy in zip(dxs, dys):
+            new_x, new_y = x + dx, y + dy
+            
+            if can_go(new_x, new_y):
+                visited_pos.append((new_x, new_y))
+                get_max_sum(cnt + 1, sum_of_nums + grid[new_x][new_y])
+                visited_pos.pop()
+    
+    return
 
-print(answer)
+
+# 격자의 각 위치에 대하여 탐색해줍니다.
+for i in range(n):
+    for j in range(m):
+        visited_pos.append((i, j))
+        get_max_sum(1, grid[i][j])
+        visited_pos.pop()
+
+print(max_sum)
